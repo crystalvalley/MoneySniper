90ba0cfd2fad52e0b29da382afef5b9d91d19477
Beomsu Kwon <qjatn9957@naver.com>
2019-06-15 19:51:03 +0900

    WebApp Env 파일 추가

---
+++ WebApp/env.js
@@ -0,0 +1,1 @@
+export const EntryPoint = "localhost:3100";
--- WebApp/node-ts/src/www.ts
+++ WebApp/node-ts/src/www.ts
@@ -2,7 +2,7 @@ import 'source-map-support/register';
 import App from './App';
 import express from 'express';
 
-const port: number = Number(process.env.PORT) || 3000;
+const port: number = Number(process.env.PORT) || 3100;
 const app: express.Application = new App().app;
 
 app.listen(port, () => console.log(`Express server listening as ${port}`))
