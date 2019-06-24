c9da1ac90badd14c89c1b0f77f7f20c8c6bb52d3
ParkHyeokjoon <hamkegaja2@gmail.com>
2019-06-24 20:16:33 +0900

    commit for merge

--- WebApp/react-ts/src/pages/App.tsx
+++ WebApp/react-ts/src/pages/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import Test from './Test';
 import Signin from './Main/Signin';
 import UserInfo from '../context/UserLoginInfoContextProvider';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import MainFrame from './Main/MainFrame';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <BrowserRouter>
       <UserInfo>
-        <Signin />
-        <br/>
-        <br/>
-        <Test />
+        <Switch>
+          <Route path="/signin" children={<Signin />} />
+          <Route path="/" children={<MainFrame/>}/>
+        </Switch>
       </UserInfo>
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ WebApp/react-ts/src/pages/Main/MainFrame.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeaderBase from './Header/HeaderBase';
+
+const MainFrame : React.FC = ()=>{
+
+    return(
+        <div>
+            <HeaderBase/>
+            Body
+        </div>
+    );
+}
+
+export default MainFrame;
